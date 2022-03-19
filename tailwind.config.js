3f14311f2865036cc1d3d4ece26ed7c9146a4ebb
spencerwooo <spencer.woo@outlook.com>
2022-03-19 20:13:10 +0800

    update tailwind css to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{ts,tsx,js,jsx}", "./index.html"],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ["./src/**/*.{ts,tsx,js,jsx}", "./index.html"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
